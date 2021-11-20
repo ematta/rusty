a79e605b63d038175262de009c8c5b16174593e6
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2021-11-20 10:14:53 -0600

    hud

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,10 +1,11 @@
-mod collisions;
 mod end_turn;
 mod entity_render;
+mod hud;
 mod map_render;
 mod movement;
 mod player_input;
 mod random_move;
+mod tooltips;
 
 use crate::prelude::*;
 
@@ -14,6 +15,8 @@ pub fn build_input_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
+        .add_system(tooltips::tooltips_system())
         .build()
 }
 
@@ -21,10 +24,9 @@ pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(collisions::collisions_system())
-        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
@@ -37,6 +39,7 @@ pub fn build_monster_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
